ebebbde0277aaf47222d3fa2ba2864c74c4b2174
Sai Kumar <sai@dora.tech>
2025-01-07 11:56:38 +0530

    added produeSync to check the err

--- kafka/service.go
+++ kafka/service.go
@@ -2,6 +2,7 @@ package kafka
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/rs/zerolog"
 	"github.com/twmb/franz-go/pkg/kgo"
@@ -15,33 +16,18 @@ func SendMessageToTopic(
 	logger zerolog.Logger,
 	topic string,
 	messages ...proto.Message,
-) {
+) error {
+	records := make([]*kgo.Record, 0)
 	for _, msg := range messages {
 		bs, err := proto.Marshal(msg)
 		if err != nil {
-			logger.Err(err).Msgf("failed to marshal transaction response: %v", msg)
-			continue
+			return fmt.Errorf("failed to marshal transaction response: %v", msg)
 		}
-		client.Produce(
-			ctx, &kgo.Record{
-				Topic: topic,
-				Value: bs,
-			}, func(r *kgo.Record, err error) {
-				if err != nil {
-					logger.Err(err).
-						Str("topic", topic).
-						Any("message", msg).
-						Msg("failed to produce transaction response record")
-					return
-				}
-
-				if logger.GetLevel() == zerolog.DebugLevel {
-					logger.Debug().
-						Str("topic", topic).
-						Any("message", msg).
-						Msg("Produced transaction response record")
-				}
-			},
-		)
+		records = append(records, &kgo.Record{
+			Topic: topic,
+			Value: bs,
+		})
 	}
+	result := client.ProduceSync(ctx, records...)
+	return result.FirstErr()
 }
