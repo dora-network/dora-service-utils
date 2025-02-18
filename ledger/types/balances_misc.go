73cf11bf736944eb8a06657fdc21b764793b121f
toteki <63419657+toteki@users.noreply.github.com>
2025-02-18 07:32:42 -0700

    add user interest tracking object

--- ledger/types/balances_misc.go
+++ ledger/types/balances_misc.go
@@ -2,9 +2,11 @@ package types
 
 import (
 	"encoding/json"
-	"github.com/dora-network/dora-service-utils/errors"
 	"regexp"
+	"strconv"
 	"strings"
+
+	"github.com/dora-network/dora-service-utils/errors"
 )
 
 // Validate that Balances does not contain any empty asset IDs
@@ -132,6 +134,12 @@ func (b *Balances) String() string {
 // ValidAssetID checks that an asset ID contains only alphanumeric characters and underscores,
 // as well as at most one hyphen somewhere in the middle, and is non-empty.
 func ValidAssetID(id string) error {
+	couponPrefix := "Coupon_"
+	if strings.HasPrefix(id, couponPrefix) {
+		// InterestSources entries do not start with coupon asset, they end with it.
+		// Individual asset IDs must not start with this prefix either, to prevent confusion.
+		return errors.Data("invalid asset ID: %s", id)
+	}
 	re := regexp.MustCompile("[^A-Za-z0-9_-]")
 	trimmed := re.ReplaceAllLiteralString(id, "")
 	if id == "" ||
@@ -140,6 +148,25 @@ func ValidAssetID(id string) error {
 		strings.HasSuffix(id, "-") ||
 		strings.Count(id, "-") > 1 {
 		return errors.Data("invalid asset ID: %s", id)
+	}
+	split := strings.Split(id, "-")
+	if len(split) == 2 {
+		// ID contained a hyphen. It is either a pool share or an InterestSources entry.
+		s := split[1]
+		if strings.HasPrefix(s, couponPrefix) {
+			// InterestSources entry rules
+			period := strings.TrimPrefix(s, couponPrefix)
+			// Period must be a valid int64 (we don't care what it is, just that it is valid)
+			if _, err := strconv.ParseInt(period, 10, 64); err != nil {
+				return errors.Data("invalid asset ID: %s", id)
+			}
+		} else {
+			// Pool share rules
+			if split[0] == split[1] {
+				// No asset can be paired with itself
+				return errors.Data("invalid asset ID: %s", id)
+			}
+		}
 	}
 	return nil
 }
