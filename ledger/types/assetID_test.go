b3299717693558dcd0ce8b368b5ee7f54fd640d3
toteki <63419657+toteki@users.noreply.github.com>
2025-02-21 09:16:24 -0700

    add module coupon funding source tracking, USD only

--- ledger/types/assetID_test.go
+++ ledger/types/assetID_test.go
@@ -12,17 +12,20 @@ func TestAssetID(t *testing.T) {
 	validAssetIDs := []string{
 		"Bond_A",
 		"USD",
-		"Bond_A-USD",        // pool share
-		"Bond_A-Coupon_123", // not an asset, but a valid Position.InterestSources entry
+		"Bond_A-USD",          // pool share
+		"Bond_A-Coupon_123",   // not an asset, but a valid Position.InterestSources entry
+		"Bond_A-Snapshot_123", // not an asset, but a valid Module.DollarCouponFundSources entry
 	}
 
 	invalidAssetIDs := []string{
 		"",
-		"A&B Co.",           // only alphanumeric characters and underscores allowed, plus up to one hyphen
-		"-abc",              // no leading hyphens
-		"abc-",              // no trailing hyphens
-		"Bond_A-USD-Bond_B", // a single hyphen indicates pool share. multiple are forbidden
-		"Coupon_123-Bond_B", // coupon period must be after the hyphen for Position.InterestSources entry
+		"A&B Co.",             // only alphanumeric characters and underscores allowed, plus up to one hyphen
+		"-abc",                // no leading hyphens
+		"abc-",                // no trailing hyphens
+		"Bond_A-USD-Bond_B",   // a single hyphen indicates pool share. multiple are forbidden
+		"Coupon_123-Bond_B",   // coupon period must be after the hyphen for Position.InterestSources entry
+		"Snapshot_123-Bond_B", // same rule; supply snapshots
+		"A-A",                 // bond paired with itself
 	}
 
 	for _, id := range validAssetIDs {
